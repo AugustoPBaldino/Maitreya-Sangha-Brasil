57edc36ce2b8f3519dacc562a61335238721d873
AugustoPB <106416779+AugustoPBaldino@users.noreply.github.com>
2024-08-03 18:00:38 -0300

    Adicionando estrutura básica para depois deixar bonito

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>Sobre nós</h1>
+      <p>Informações sobre o escritório e nossa equipe.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
