89ac7316d1db006b20680c08e4f199ba17abb90e
AugustoPB <106416779+AugustoPBaldino@users.noreply.github.com>
2024-08-05 16:29:35 -0300

    Criando layout de página única

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
+import Navbar from './components/Navbar';
+import Section from './Section/Section';
+import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/sobre" element={<AboutPage />} />
-        <Route path="/contato" element={<ContactPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <Navbar />
+      <Section id="home" title="Home">
+        Conteúdo da seção Home...
+      </Section>
+      <Section id="about" title="Sobre Nós">
+        Maitreya Sangha Brasil é uma comunidade espiritual dedicada ao estudo e à prática dos ensinamentos budistas de Maitreya, o Buda do Futuro. Nossa missão é promover a paz interior e a compaixão através da meditação, estudo, e ações altruístas. Oferecemos um espaço acolhedor para pessoas de todas as origens, interessados em aprofundar sua compreensão do Dharma e em cultivar uma vida de sabedoria e compaixão.
+      </Section>
+      <Section id="services" title="Serviços">
+        Conteúdo da seção Serviços...
+      </Section>
+      <Section id="contact" title="Contato">
+        Conteúdo da seção Contato...
+      </Section>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">Sobre Nós</a></li>
+        <li><a href="#services">Serviços</a></li>
+        <li><a href="#contact">Contato</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
