e03aaf6aa54f9c0468c7bfeb6b1f37d5feb4898b
Your Name <you@example.com>
2024-08-09 13:57:24 -0300

    Adicionando textos da página e estilizando a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,15 @@ const App = () => {
         Maitreya Sangha Brasil é uma comunidade espiritual dedicada ao estudo e à prática dos ensinamentos budistas de Maitreya, o Buda do Futuro. Nossa missão é promover a paz interior e a compaixão através da meditação, estudo, e ações altruístas. Oferecemos um espaço acolhedor para pessoas de todas as origens, interessados em aprofundar sua compreensão do Dharma e em cultivar uma vida de sabedoria e compaixão.
       </Section>
       <Section id="services" title="Serviços">
-        Conteúdo da seção Serviços...
+<h4>Eventos</h4>
+<p>Nossa comunidade organiza uma variedade de eventos voltados para a prática espiritual e o fortalecimento da comunidade. Oferecemos retiros de meditação, 
+  workshops de desenvolvimento pessoal, e celebrações especiais que promovem a paz interior e o crescimento espiritual.</p>
+<h4>Lançamentos</h4>
+<p>Estamos sempre buscando novas formas de compartilhar o Dharma e enriquecer a jornada espiritual de nossos membros. Confira nossos lançamentos recentes e futuros, incluindo publicações, podcasts, e outros materiais inspiradores.</p>
+<h4>Cursos</h4>
+<p>Oferecemos cursos regulares que abordam diferentes aspectos dos ensinamentos budistas e da meditação. Desde iniciantes até praticantes avançados, nossos cursos são projetados para apoiar o crescimento espiritual de todos.</p>
+<h4>Entrevistas</h4>
+<p>Nossas entrevistas com mestres, praticantes e estudiosos do Dharma trazem insights valiosos sobre a prática budista e a aplicação dos ensinamentos no dia a dia. Acompanhe nossas conversas para se inspirar e aprofundar seu entendimento do caminho espiritual.</p>
       </Section>
       <Section id="contact" title="Contato">
         Conteúdo da seção Contato...
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import logo from '/home/augusto/Projects/Maitreya-Sangha-Brasil/src/assets/logo01.png'; // Substitua pelo caminho da sua imagem
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+        <span className="site-name">Maitreya Sangha Brasil</span>
+      </div>
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">Sobre Nós</a></li>
