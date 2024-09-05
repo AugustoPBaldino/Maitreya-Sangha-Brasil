54089f71b9cc28f992b5f2834a5ac1dd305bb7a1
Your Name <you@example.com>
2024-09-05 19:36:26 -0300

    Innitial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var swiper = new Swiper('.swiper', {
+//         slidesPerView: 1, /* Mostra um card por vez */
+//         spaceBetween: 20,
+//         loop: true, /* Faz o carrossel rodar continuamente */
+//         pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//         },
+//         navigation: {
+//             nextEl: '.swiper-button-next',
+//             prevEl: '.swiper-button-prev',
+//         },
+//     });
+// });
