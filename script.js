3e6886df5cb77201d3d28d529aa1b9ca3b8564f5
Your Name <you@example.com>
2024-09-10 10:23:31 -0300

    Ajustando estrutura da história do Lama, Colocando legendas e arrumando parcialmente os vídeos das entrevistas

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var swiper = new Swiper('.swiper', {
-//         slidesPerView: 1, /* Mostra um card por vez */
-//         spaceBetween: 20,
-//         loop: true, /* Faz o carrossel rodar continuamente */
-//         pagination: {
-//             el: '.swiper-pagination',
-//             clickable: true,
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3, /* Exibir 3 vídeos por vez */
+//     spaceBetween: 30, /* Espaço entre os vídeos */
+//     loop: true, /* Habilita loop */
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: {
+//         768: {
+//             slidesPerView: 2, /* Mostra 2 vídeos por vez em telas menores */
 //         },
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//         },
-//     });
+//         480: {
+//             slidesPerView: 1, /* Mostra 1 vídeo por vez em telas pequenas */
+//         }
+//     }
 // });
