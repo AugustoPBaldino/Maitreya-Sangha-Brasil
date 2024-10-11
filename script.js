b2939e8ddeae4af637bdc52b638688b590987f25
Your Name <you@example.com>
2024-10-10 22:14:41 -0300

    Arrumei a porra do swipper graças  deus

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 3, /* Exibir 3 vídeos por vez */
-//     spaceBetween: 30, /* Espaço entre os vídeos */
-//     loop: true, /* Habilita loop */
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     breakpoints: {
-//         768: {
-//             slidesPerView: 2, /* Mostra 2 vídeos por vez em telas menores */
-//         },
-//         480: {
-//             slidesPerView: 1, /* Mostra 1 vídeo por vez em telas pequenas */
-//         }
-//     }
-// });
-document.querySelectorAll('nav a').forEach(anchor => {
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1, // Exibe apenas um slide por vez
+    spaceBetween: 0, // Remove qualquer espaço entre os slides
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+  document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
-      e.preventDefault(); // Previne o comportamento padrão do link
+      e.preventDefault();
       const targetId = this.getAttribute('href');
       document.querySelector(targetId).scrollIntoView({
         behavior: 'smooth'
       });
     });
   });
-  
 
   window.addEventListener("scroll", () => {
     const parallax = document.querySelector(".home");
     let scrollPosition = window.pageYOffset;
     parallax.style.backgroundPositionY = scrollPosition * 0.5 + "px";
+  });
 });
+
 
 
